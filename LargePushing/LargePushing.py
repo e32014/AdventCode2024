bc9392c241fa4af9a0d3068f9179d8fb97a3d661
Evan Klei <e32014@gmail.com>
2024-12-15 01:05:53 -0500

    Day 15: Simulatino with a twist. Use a small BFS to hunt across boxes to fix simulation of only left sides. Originally I had a separate functions for horizontal vs vertical, but it ended up easier to just handle them the same. Not proud of having 4 different sorts, but with set based box positions, I needed to make sure we handled them from furthest to closest.

---
+++ LargePushing/LargePushing.py
@@ -0,0 +1,77 @@
+file = open('input.txt')
+
+dirs = {"<": (-1, 0), "^": (0, -1), "v": (0, 1), ">": (1,0)}
+walls = set()
+boxes = set()
+box_pair = {}
+curr = ()
+commands = ""
+mode = 1
+max_x = 0
+max_y = 0
+for i, line in enumerate(file):
+    if line == "\n":
+        mode = 2
+        continue
+    if mode == 1:
+        max_y += 1
+        for j, char in enumerate(line.strip()):
+            if char == "#":
+                walls.add((j * 2, i))
+                walls.add((j * 2 + 1, i))
+            if char == "O":
+                boxes.add((j * 2, i))
+            if char == "@":
+                curr = (j * 2, i)
+        max_x = (j + 1) * 2
+    elif mode == 2:
+        commands += line.strip()
+
+for command in commands:
+    dx, dy = dirs[command]
+    px, py = curr
+
+    hit_boxes = []
+    offset = 1
+
+    consider = [(px, py)]
+    visited = []
+    while consider:
+        cx, cy = consider.pop()
+        if (cx, cy) in visited:
+            continue
+        visited.append((cx, cy))
+        if (cx + dx, cy + dy) in boxes:
+            consider.append((cx + dx, cy + dy))
+            consider.append((cx + dx + 1, cy + dy))
+        elif (cx + dx - 1, cy + dy) in boxes:
+            consider.append((cx + dx - 1, cy + dy))
+            consider.append((cx + dx, cy + dy))
+    legal = True
+    for vx, vy in visited:
+        if (vx + dx, vy + dy) in walls:
+            legal = False
+            break
+        elif (vx, vy) in boxes:
+            hit_boxes.append((vx, vy))
+
+    if not legal:
+        continue
+
+    if dir == '<':
+        hit_boxes.sort(key=lambda x:x[0])
+    elif dir == '>':
+        hit_boxes.sort(key=lambda x:x[0], reverse=True)
+    elif dir == '^':
+        hit_boxes.sort(key=lambda x:x[1])
+    elif dir == 'v':
+        hit_boxes.sort(key=lambda x:x[1], reverse=True)
+    for box in hit_boxes:
+        boxes.remove(box)
+        boxes.add((box[0] + dx, box[1] + dy))
+    curr = (px + dx, py + dy)
+
+total = 0
+for bx, by in boxes:
+    total += by * 100 + bx
+print(total)
